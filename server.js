24ba3a599b223ecbf5bc8fd17d1ff43e0c32720d
Chris Lynch (vVv F R 0 O T x) <thefad3@fullsail.edu>
2015-06-15 04:32:24 +0000

    Because Cloud9 is BEING SKETCHY

--- server.js
+++ server.js
@@ -1,10 +1,10 @@
 var http = require('http');
 var path = require('path');
-
 var async = require('async');
 var socketio = require('socket.io');
 var express = require('express');
-var Camera = require('gopro').Camera
+var Camera = require('gopro').Camera;
+var ejs = require('ejs');
 
 
 var app= express();
@@ -12,16 +12,17 @@ var server = http.createServer(app);
 var io = socketio.listen(server);
 
 app.use(express.static(path.resolve(__dirname, 'client')));
+app.set('view engine', 'ejs');
 
 
 app.get('/', function(req, res){
-  
-  var cam = new Camera('10.5.5.9', 'camera password')
+
+  //var cam = new Camera('10.5.5.9', 'camera password')
+  res.render('index');
 
 });
 
 
 server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function(){
   var addr = server.address();
-  console.log("Chat server listening at", addr.address + ":" + addr.port);
 });
